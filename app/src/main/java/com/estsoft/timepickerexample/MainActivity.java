dac8aa9707a7f045cfaa752eac818f2fa7229d2f
yattadog <kickscar@gmail.com>
2016-11-25 06:34:55 +0900

    수정

--- app/src/main/java/com/estsoft/timepickerexample/MainActivity.java
+++ app/src/main/java/com/estsoft/timepickerexample/MainActivity.java
@@ -17,22 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         datePicker = (DatePicker)findViewById( R.id.datePicker );
-        datePicker.init( 2016, 5-1, 24, new DatePicker.OnDateChangedListener() {
-            @Override
-            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                ((TextView)findViewById( R.id.dateText )).setText( year + "-" + ( monthOfYear + 1 ) + "-"+ dayOfMonth );
-            }
-        });
 
-        findViewById( R.id.button ).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int year = datePicker.getYear();
-                int month = datePicker.getMonth() + 1;
-                int day = datePicker.getDayOfMonth();
-
-                Toast.makeText(MainActivity.this, year + "년 " + month + "월 " + day + "일", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
